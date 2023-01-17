4b6eaf36ff21d9e12157d3b607e54ff7cd8500f1
Nicolas Pham <phd.ngoc163@gmail.com>
2023-01-17 20:11:06 +0000

    Update 3 files

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 
-const Header = () => {
+const Header = ({ page }) => {
   //////////////////////////////////////////////
   const handleActive = (e) => {
     console.log(e);
@@ -39,12 +39,16 @@ const Header = () => {
     children: 0,
     room: 1,
   });
-
-  const addAdult = () => {
-    setOptions((options.adult += 1));
-    console.log(options);
-  };
   const [openOptions, setOpenOptions] = useState(false);
+
+  const handleOptions = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
 
   //////////////////////////////////////////////
   return (
@@ -61,75 +65,100 @@ const Header = () => {
           </div>
         ))}
       </div>
-      <div className="headerContent">
-        <h1>A lifetime of discounts? It's Genius</h1>
-        <p>
-          Get rewarded for your travels - unlock instant savings of 10% or more
-          with a free Nicolas Booking account
-        </p>
-        <button>Sign in / Register</button>
-      </div>
-      <div className="flex al-center headerSearch">
-        <div className="searchItem">
-          <FontAwesomeIcon icon={faBed} className="icon" />
-          <input type="text" placeholder="Where are you going?" />
-        </div>
-        <div className="searchItem">
-          <FontAwesomeIcon icon={faCalendarDays} className="icon" />
-          <span onClick={() => setOpenDate(!openDate)}>{`${format(
-            date[0].startDate,
-            "MM/dd/yyyy"
-          )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
-        </div>
-        {openDate && (
-          <DateRange
-            editableDateInputs={true}
-            onChange={(item) => setDate([item.selection])}
-            moveRangeOnFirstSelection={false}
-            ranges={date}
-            className="date"
-          />
-        )}
-        <div className="searchItem">
-          <FontAwesomeIcon icon={faPerson} className="icon" />
-          <span onClick={() => setOpenOptions(!openOptions)}>
-            2 adults 2 children 2 rooms
-          </span>
-          {openOptions && (
-            <div className="flex-column options">
-              <div className="item">
-                <span>Adult</span>
-                <div className="adjust">
-                  <button onClick={() => setOptions(options.adult - 1)}>
-                    -
-                  </button>
-                  <span>{options.adult}</span>
-                  <button onClick={addAdult}>+</button>
-                </div>
-              </div>
-              <div className="item">
-                <span>Children</span>
-                <div className="adjust">
-                  <button>-</button>
-                  <span>{options.children}</span>
-                  <button>+</button>
-                </div>
-              </div>
-              <div className="item">
-                <span>Room</span>
-                <div className="adjust">
-                  <button>-</button>
-                  <span>{options.room}</span>
-                  <button>+</button>
-                </div>
-              </div>
+
+      {page !== "list" && (
+        <>
+          <div className="headerContent">
+            <h1>A lifetime of discounts? It's Genius</h1>
+            <p>
+              Get rewarded for your travels - unlock instant savings of 10% or
+              more with a free Nicolas Booking account
+            </p>
+            <button>Sign in / Register</button>
+          </div>
+          <div className="flex al-center headerSearch">
+            <div className="searchItem">
+              <FontAwesomeIcon icon={faBed} className="icon" />
+              <input type="text" placeholder="Where are you going?" />
             </div>
-          )}
-        </div>
-        <div className="searchItem">
-          <button>Search</button>
-        </div>
-      </div>
+            <div className="searchItem">
+              <FontAwesomeIcon icon={faCalendarDays} className="icon" />
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "MM/dd/yyyy"
+              )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+            </div>
+            {openDate && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+              />
+            )}
+            <div className="searchItem">
+              <FontAwesomeIcon icon={faPerson} className="icon" />
+              <span onClick={() => setOpenOptions(!openOptions)}>
+                {`${options.adult} adult - ${options.children} children - ${options.room} room`}
+              </span>
+
+              {openOptions && (
+                <div className="flex-column options">
+                  <div className="item">
+                    <span>Adult</span>
+                    <div className="adjust">
+                      <button
+                        disabled={options.adult <= 1}
+                        onClick={() => handleOptions("adult", "d")}
+                      >
+                        -
+                      </button>
+                      <span>{options.adult}</span>
+                      <button onClick={() => handleOptions("adult", "i")}>
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <span>Children</span>
+                    <div className="adjust">
+                      <button
+                        disabled={options.children <= 0}
+                        onClick={() => handleOptions("children", "d")}
+                      >
+                        -
+                      </button>
+                      <span>{options.children}</span>
+                      <button onClick={() => handleOptions("children", "i")}>
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <span>Room</span>
+                    <div className="adjust">
+                      <button
+                        disabled={options.room <= 1}
+                        onClick={() => handleOptions("room", "d")}
+                      >
+                        -
+                      </button>
+                      <span>{options.room}</span>
+                      <button onClick={() => handleOptions("room", "i")}>
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="searchItem">
+              <button>Search</button>
+            </div>
+          </div>
+        </>
+      )}
     </section>
   );
 };
