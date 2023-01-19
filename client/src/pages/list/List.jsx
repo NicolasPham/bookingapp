1e784c99d264a01073647e92fc18f4830e4ef3e4
NicolasPham <67529386+NicolasPham@users.noreply.github.com>
2023-01-19 16:17:11 +0000

    updated

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/Header/Header";
+import Post from "../../components/post/Post";
+
+import "./list.scss";
+
+//------- Date picker ---------//
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
 
 const List = () => {
+  //--------------------------------------------//
+  
+
+
+  //--------------------------------------------//
   return (
-    <div>
+    <div class="list">
       <Navbar />
       <Header page="list" />
 
@@ -12,9 +26,48 @@ const List = () => {
         <div className="wrapper">
           <section className="search">
             <h1>Search</h1>
-            <form></form>
+            <form className="flex-column form">
+              <label htmlFor="destination">Destination</label>
+              <input type="text" placeholder="Ex: Toronto, etc" />
+
+              <label htmlFor="checkinDate">Check-in Date</label>
+              <span className="date">date to date</span>
+              <div className="flex-column options">
+                <span className="title">Options</span>
+
+                <div className="option">
+                <label htmlFor="minprice">Min price (per night)</label>
+                <input type="text" id="minprice" />
+                </div>
+                <div className="option">
+                <label htmlFor="maxprice">Max price (per night)</label>
+                <input type="text" id="maxprice" />
+                </div>
+                <div className="option">
+                <label htmlFor="adult">Adult</label>
+                <input type="text" id="adult" />
+                </div>
+                <div className="option">
+                <label htmlFor="children">Children</label>
+                <input type="text" id="children" />
+                </div>
+                <div className="option">
+                <label htmlFor="room">Room</label>
+                <input type="text" id="room" />
+                </div>
+              </div>
+
+
+              <button>Search</button>
+            </form>
           </section>
-          <section className="result"></section>
+
+
+          <section className="result">
+            <Post/>
+
+          </section>
+
         </div>
       </main>
     </div>
