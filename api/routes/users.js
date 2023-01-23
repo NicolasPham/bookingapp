d9973c037c42e0a912d7bc0a7986e9eec51624fd
NicolasPham <67529386+NicolasPham@users.noreply.github.com>
2023-01-23 19:17:37 +0000

    updated

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,17 +1,9 @@
 import express from 'express';
-import * as user from '../controllers/user.js';
 
 
 const router = express.Router();
 
-router.post("/register", user.register);
-router.post("/login", user.login);
 
-router.get('/all', user.getAll)
-
-router.delete("/:id", user.deleteUser);
-
-router.get('/', user.test)
-
-
-export default router
+router.get('/', (req, res) => {
+    res.send("User page")
+})
