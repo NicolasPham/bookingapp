19e1de687fdf6835a10e183a0a7cbb4615a3f95e
Nicolas Pham <phd.ngoc163@gmail.com>
2023-01-25 12:57:00 +0000

    Update 5 files

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,22 +1,24 @@
-import express from 'express';
-import * as auth from '../controllers/auth.js';
-import { verifyToken } from '../utils/verifyToken.js';
-
+import express from "express";
+import * as auth from "../controllers/auth.js";
+import { verifyAdmin, verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/register", auth.register);
 router.post("/login", auth.login);
 
-router.get('/all', auth.getAll)
+router.get("/all", verifyToken, verifyAdmin, auth.getAll);
 
-router.delete("/:id", auth.deleteUser);
+router.get("/", auth.test);
 
-router.get('/', auth.test)
+// router.get("/check", verifyToken, (req, res, next) => {
+//   res.send("Hello user, you are logged in");
+// });
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello user, you can delete account");
+// });
+// router.get("/checkadmin", verifyAdmin, (req, res) => {
+//   res.send("Hello admin");
+// });
 
-router.get("/check", verifyToken, (req, res, next) => {
-    res.send("Hello user, you are logged in")
-})
-
-
-export default router
+export default router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import * as user from '../controllers/user.js';
-
+import express from "express";
+import * as user from "../controllers/user.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-
-router.put('/:id', user.updateUser);
-router.delete("/:id", user.deleteUser);
+router.put("/:id", verifyToken, verifyUser, user.updateUser);
+router.delete("/:id", verifyToken, verifyUser, user.deleteUser);
 
 export default router;
