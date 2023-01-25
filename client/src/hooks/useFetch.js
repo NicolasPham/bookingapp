302e4d053de96aacf8b97e472e14106d2b4e8e6d
Nicolas Pham <phd.ngoc163@gmail.com>
2023-01-25 17:40:43 +0000

    Update 3 files

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,7 @@
+const { useState } = require("react/cjs/react.production.min");
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+};
