311b26b010699cf98b80464980ed38fb23093921
NicolasPham <phd.ngoc163@gmail.com>
2023-02-01 21:32:30 -0500

    update

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 /*****Router imported *****/
 import authRoutes from "./routes/auth.js";
